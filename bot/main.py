418276d393cc22cc277e2e057d28e82e53ed1864
Michael <misharad2008@gmail.com>
2023-06-29 23:21:48 +0300

    refactor code

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,13 @@
-from handlers import client
+from handlers import utils
 from dp_init import dp
 from aiogram import executor
 
 
 def start_up():
-    print('[LOG:] Bot is started')
+    print('Bot is started')
 
 
-client.register_handlers_client(dp)
+utils.register_handlers_client(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=start_up())
